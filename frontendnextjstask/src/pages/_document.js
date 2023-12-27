5504ba2e27ac3556a5de24d30d8869d383a57ac0
Saphal Sherchan <saphalsherchan10@gmail.com>
2023-12-27 13:05:10 +0545

    updated title of OnlineStore

--- frontendnextjstask/src/pages/_document.js
+++ frontendnextjstask/src/pages/_document.js
@@ -18,6 +18,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css"
         />
+        <title>OnlineStore</title>
       </Head>
       <body>
         <Main />
